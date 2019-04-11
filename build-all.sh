f4272e8a9c3adfabc8481a5405aa3d64f9ca163b
WEI Yongming <vincent@minigui.org>
2019-04-11 18:19:06 +0800

    tune

--- build-all.sh
+++ build-all.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 runmode=ths
-nr_jobs=8
+nr_jobs=16
 
 cd minigui
 ./autogen.sh; ./configure --enable-develmode --with-runmode=$runmode
 make -j$nr_jobs; sudo make install
 cd ..
 
-for comp in minigui-res mg-tests mgutils mgplus mgeff mgncs mgncs4touch mg-samples mg-tools cell-phone-ux-demo; do
+for comp in minigui-res mg-tests mgutils mgplus mgeff mgncs mgncs4touch mg-samples cell-phone-ux-demo; do
     cd $comp
     ./autogen.sh; ./configure --enable-develmode
     make -j$nr_jobs; sudo make install
