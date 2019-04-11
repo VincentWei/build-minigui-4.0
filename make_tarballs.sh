02064658c3c94ef03e5d2b1a4e77f0ac349e215c
Vincent Wei <vincent@minigui.org>
2019-04-11 14:35:58 +0800

    make_tarballs.sh

---
+++ make_tarballs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo MAKE TARBALLS OF MINIGUI DOCS
+for comp in minigui; do
+    cd $comp
+    make docs
+    for name in doc-*; do
+        tar czf $name.tar.gz $name
+    done
+    mv *.tar.gz ..
+    cd ..
+done
+
+echo MAKE TARBALLS OF GVFB
+cd gvfb
+make package_source
+mv *.tar.gz ..
+cd ..
+
+echo MAKE TARBALLS OF MINIGUI
+for comp in minigui minigui-res mgutils mgplus mgeff mgncs mg-tests mg-samples mgncs4touch cell-phone-ux-demo; do
+    cd $comp
+    make dist
+    mv *.tar.gz ..
+    cd ..
+done
+
