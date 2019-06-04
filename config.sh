cdc3e2c758807c7181df604070a2880f60d9a091
Vincent Wei <vincent@minigui.org>
2019-06-04 12:31:57 +0800

    ignore config.sh

--- config.sh
+++ config.sh
@@ -3,8 +3,8 @@
 
 # The URL prefix of remote repository.
 # Recommend to use github.com; The gitlab URL is only for developers.
-REPO_URL=git@github.com:VincentWei
-#REPO_URL=git4os@gitlab.fmsoft.cn:VincentWei
+#REPO_URL=git@github.com:VincentWei
+REPO_URL=git4os@gitlab.fmsoft.cn:VincentWei
 
 # The runtime mode of MiniGUI; can be ths, procs, or sa.
 RUNMODE=ths
