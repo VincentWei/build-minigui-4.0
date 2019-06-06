4f5c02c680a19fe68becd90a990a1af5e8564191
Vincent Wei <vincent@minigui.org>
2019-06-06 11:41:38 +0800

    HTTPS URL; some comments

--- config.sh
+++ config.sh
@@ -1,13 +1,25 @@
-# This file will be included in every scripts.
-# Please change the following global variables for customization.
+# This is the template of `myconfig.sh`, and the later
+# will be included in every script.
 
+# PLEASE COPY THIS FILE TO `myconfig.sh`,
+# AND CHANGE THE FOLLOWING GLOBAL VARIABLES FOR YOUR CUSTOMIZATION.
+
+#
 # The URL prefix of remote repository.
-# Recommend to use github.com; The gitlab URL is only for developers.
-REPO_URL=git@github.com:VincentWei
+
+# Use this if you want to visit GitHub via HTTPS
+REPO_URL=https://github.com/VincentWei
+
+# Use this one if you can visit GitHub via SSH
+#REPO_URL=git@github.com:VincentWei
+
+# Use this one if you are a developer of MiniGUI
 #REPO_URL=git4os@gitlab.fmsoft.cn:VincentWei
 
+#
 # The runtime mode of MiniGUI; can be ths, procs, or sa.
 RUNMODE=ths
 
+#
 # The jobs number when building source.
 NR_JOBS=8
