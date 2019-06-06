7dbe347542e4b2956657a05fd818d72d7c4e7111
Vincent Wei <vincent@minigui.org>
2019-06-06 16:45:32 +0800

    call "make clean"

--- build-all.sh
+++ build-all.sh
@@ -11,12 +11,12 @@ nr_jobs=$NR_JOBS
 
 cd minigui
 ./autogen.sh; ./configure --enable-develmode --with-runmode=$runmode
-make -j$nr_jobs; sudo make install
+make clean; make -j$nr_jobs; sudo make install
 cd ..
 
 for comp in minigui-res mg-tests mgutils mgplus mgeff mgncs mgncs4touch mg-tools mg-samples mg-demos cell-phone-ux-demo; do
     cd $comp
     ./autogen.sh; ./configure --enable-develmode
-    make -j$nr_jobs; sudo make install
+    make clean; make -j$nr_jobs; sudo make install
     cd ..
 done
