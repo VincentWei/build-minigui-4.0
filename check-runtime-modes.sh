fd9482c99d3021878dd340d1a76444542f9c22e9
Vincent Wei <vincent@minigui.org>
2020-01-08 14:42:04 +0800

    tune output information

--- check-runtime-modes.sh
+++ check-runtime-modes.sh
@@ -6,60 +6,76 @@ fi
 
 source myconfig.sh
 
+RUNMODE=
+
 compile_components() {
     for comp in mgutils mgplus mgeff mgncs mgncs4touch mg-tests mg-tools mg-samples mg-demos cell-phone-ux-demo; do
         cd $comp
         ./configure && make clean && make -j$NR_JOBS && sudo make install
         if [ "$?" != "0" ]; then
-            echo "ERROR WHEN COMPILING $comp"
+            echo "====="
+            echo "ERROR WHEN COMPILING $comp FOR $RUMMODE"
+            echo "====="
             exit 1
         fi
         cd ..
     done
 }
 
-echo "MINIGUI THREADS"
+RUNMODE="MINIGUI THREADS"
 cd minigui
 ./configure --with-runmode=ths --enable-develmode && make clean && make -j$NR_JOBS && sudo make install
 if [ "$?" != "0" ]; then
-    echo "ERROR WHEN COMPILING MINIGUI-THREADS"
+    echo "====="
+    echo "ERROR WHEN COMPILING minigui FOR $RUNMODE"
+    echo "====="
     exit 1
 fi
 cd ..
 
 compile_components
 
-echo "MINIGUI STANDALONE"
+RUNMODE="MINIGUI STANDALONE"
 cd minigui
 ./configure --with-runmode=sa --enable-develmode && make clean && make -j$NR_JOBS && sudo make install
 if [ "$?" != "0" ]; then
-    echo "ERROR WHEN COMPILING MINIGUI-STANDALONE"
+    echo "====="
+    echo "ERROR WHEN COMPILING minigui FOR $RUNMODE"
+    echo "====="
     exit 1
 fi
 cd ..
 
 compile_components
 
-echo "MINIGUI PROCESSES WITH SHAREDFB SCHEMA"
+RUNMODE="MINIGUI PROCESSES WITH SHAREDFB SCHEMA"
 cd minigui
 ./configure --with-runmode=procs --disable-compositing --enable-develmode && make clean && make -j$NR_JOBS && sudo make install
 if [ "$?" != "0" ]; then
-    echo "ERROR WHEN COMPILING MINIGUI-PROCESSES WITH SHAREDFB SCHEMA"
+    echo "====="
+    echo "ERROR WHEN COMPILING minigui FOR $RUNMODE"
+    echo "====="
     exit 1
 fi
 cd ..
 
 compile_components
 
-echo "MINIGUI PROCESSES WITH COMPOSITING SCHEMA"
+RUNMODE="MINIGUI PROCESSES WITH COMPOSITING SCHEMA"
 cd minigui
-./configure --with-runmode=procs --enable-develmode && make clean && make -j$NR_JOBS && sudo make install
+./configure --with-runmode=procs --enable-compositing --enable-develmode && make clean && make -j$NR_JOBS && sudo make install
 if [ "$?" != "0" ]; then
-    echo "ERROR WHEN COMPILING MINIGUI-PROCESSES WITH SHAREDFB SCHEMA"
+    echo "====="
+    echo "ERROR WHEN COMPILING minigui FOR $RUNMODE"
+    echo "====="
     exit 1
 fi
 cd ..
 
 compile_components
+
+echo "====="
+echo "PASSED"
+echo "====="
 
 exit 0
